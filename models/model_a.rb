93b0d738bdbfc7d40f5e932a7e2f50d4b01e0fe4
Jessica Grazulis <jlgrazulis@gmail.com>
2021-09-03 20:39:07 -0600

    Add dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,13 @@
+dinosaur = {
+  "type" => 'Velociraptor',
+  "feathers" => true,
+  "scales" => false,
+  "name" => 'Buddy',
+  "height_in_feet" => 6,
+  "weight_in_pounds" => 33,
+  "colors" => ['blue', 'orange', 'grayl'],
+  "related_to" => ['turkeys', 'chickens', 'other flightless birds'],
+  "travel_method" => ['in packs', 4, 'family'],
+  "prey" => ['lots of other dinosaurs', 'fish', 'king crab'],
+  "carnivore" => true
+}
