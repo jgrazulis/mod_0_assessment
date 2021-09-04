dc6d3f11d08140b7ca9d147868d00c61b58346a2
Jessica Grazulis <jlgrazulis@gmail.com>
2021-09-03 20:07:30 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Harry Potter and the Prisoner of Azkaban"
+is_entertaining = true
+movie = "Harry Potter and the Sorcers Stone"
+num_4 = 4
+num_6 = 6
+product = (num_6 * num_4)
+favorite_foods = ["Mashed Potatoes", "Macaroni and Cheese", "Pad Thai"]
+favorite_foods.shift
+movie = movie.count("characters")
