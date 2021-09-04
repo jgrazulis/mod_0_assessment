9de1c6f09d6b36aecc6256e7dbb326bc55e55a8d
Jessica Grazulis <jlgrazulis@gmail.com>
2021-09-03 21:13:25 -0600

    Add developer

---
+++ models/model_b.rb
@@ -0,0 +1,13 @@
+developer = {
+  "name" => 'Jessica',
+  "age" => 34,
+  "height_in_feet" => 5'6,
+  "learning_ruby" => true,
+  "learning_JavaScript" => false,
+  "favorite_things_to_do" => ['hashes, git'],
+  "schools_attended" => ['Aurora University', 'University of Denver', 'Joliet Junior College'],
+  "excited_for" => growth: ["as person", "as developer", "meeting new people"]
+                   learning: ["Ruby", "tips and tricks", "how to google well"],
+  "nervous about" => code: ["getting stuck", "nervous breakdowns"],
+                      personal: ["finding time with partner", "prioritizing wellbeing"]
+}
